41881e187c2b1971a8467a186920a03d900e738c
Unai Leon Plaza <uleon002@ikasle.ehu.eus>
2024-11-13 17:00:59 +0100

    pagina mejor !!!

--- public/js/skillspecifics.js
+++ public/js/skillspecifics.js
@@ -1,11 +1,18 @@
-function loadHexagon() {
+function loadInformation() {
     const skillHexagon = localStorage.getItem('skillHexagon');
     if (skillHexagon) {
         const svgContainer = document.createElement('div');
         svgContainer.classList.add('svg-container');
         svgContainer.innerHTML = skillHexagon;
-        document.querySelector('.skill-details').appendChild(svgContainer);
+        document.querySelector('.hexagon-container').appendChild(svgContainer);
     }
+
+    const title = document.getElementById('title');
+    const description = document.getElementById('skill-description');
+    const infoColumn = document.querySelector('.info-column');
+
+    infoColumn.appendChild(title);
+    infoColumn.appendChild(description);
 }
 
-window.onload = loadHexagon;
+window.onload = loadInformation;
